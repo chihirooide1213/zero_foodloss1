9a3f38dc7999f13634cac36e5957a7f0105dbaf4
vagrant <vagrant@localhost.localdomain>
2020-09-07 04:14:33 +0000

    add_model

---
+++ app/controllers/clients/genres_controller.rb
@@ -0,0 +1,7 @@
+class Clients::GenresController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+end
---
+++ app/controllers/clients/items_controller.rb
@@ -0,0 +1,4 @@
+class Clients::ItemsController < ApplicationController
+  def index
+  end
+end
