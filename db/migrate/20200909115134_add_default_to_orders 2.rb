f52df7aa3f56b6c9a3e402d58c411fe8fea8cd66
vagrant <vagrant@localhost.localdomain>
2020-09-20 06:56:13 +0000

    fix_name

--- db/migrate/20200909115134_add_default_to_orders 2.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultToOrders < ActiveRecord::Migration[5.2]
-  def change
-  	change_column_default :orders, :shipping_cost, default: '800'
-  	change_column_default :orders, :status, default: '1'
-  end
-end
